19c1f52e5473691da3594c4470175496452bb271
boylee8713 <105021061@live.asia.edu.tw>
2016-10-24 15:52:57 +0800

    交

---
+++ src/hw/hw03_105021061.java
@@ -0,0 +1,36 @@
+package hw;
+
+import java.util.*;
+
+public class hw03_105021061 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner src = new Scanner(System.in);
+		System.out.println("輸入西元年:");
+		int a = src.nextInt();
+		
+		if(a%4==0)
+		{
+			if(a%100==0)
+			{
+				if(a%400==0)
+				{
+					System.out.print("閏年");
+				}
+				else
+				{
+					System.out.print("平年");
+				}
+			}
+			else
+			{
+				System.out.print("閏年");
+			}
+		}
+		else
+		{
+			System.out.print("平年");
+		}
+	}
+}
